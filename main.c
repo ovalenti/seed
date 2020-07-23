1a9c2dec60b28d8e7313f649b1effc8fdd0d7030
Olivier Valentin <valentio@free.fr>
2020-07-23 17:03:57 +0200

    Implement trivial heap allocator

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 #include "interpreter.h"
 #include "basic_io.h"
 
+static char heap[2 * 1024];
+
 static int extra_builtin_basic_io(char **pos) {
 	if (eat("[INC]", pos)) {
 		stack[stack_ptr++] = basic_io_inc();
@@ -22,5 +24,6 @@ static int extra_builtin_basic_io(char **pos) {
 
 int main() {
 	extra_builtin = &extra_builtin_basic_io;
+	*scope_find("{HEAP}" + 1) = (unsigned long)heap;
 	execute_at(program);
 }
