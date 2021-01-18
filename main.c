c405f3a65c14fa6b9fff72b476679a2f33576f54
Olivier Valentin <valentio@free.fr>
2021-01-18 14:41:04 +0100

    Proof of concept generating binary from seed code
    Implement OUTC as a linux syscall on x86_64

--- main.c
+++ main.c
@@ -15,14 +15,23 @@ static char heap[2 * 1024];
 static int extra_builtin_basic_io(char **pos) {
 	if (eat("[INC]", pos)) {
 		stack[stack_ptr++] = basic_io_inc();
-	} else if (eat("[OUTC]", pos)) {
-		basic_io_outc(stack[--stack_ptr]);
+//	} else if (eat("[OUTC]", pos)) {
+//		basic_io_outc(stack[--stack_ptr]);
 	} else
 		return 0;
 	return !0;
 }
 
+#include <sys/mman.h>
+#include <unistd.h>
+void fix_heap() {
+	long ps = sysconf(_SC_PAGE_SIZE);
+	unsigned long mask = ~(ps - 1);
+	mprotect((void*)((unsigned long)heap & mask), (sizeof(heap) & mask) + ps, PROT_READ | PROT_WRITE | PROT_EXEC);
+}
+
 int main(void) {
+	fix_heap();
 	extra_builtin = &extra_builtin_basic_io;
 	*scope_find("{HEAP}" + 1, 0) = (unsigned long)heap;
 	execute_at(program);
